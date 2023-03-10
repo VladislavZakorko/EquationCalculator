1e30fe05eb8db53841f8882e81c8237897986d93
Vladislav Zakorko <vladzakorko@gmail.com>
2023-03-10 18:16:25 +0300

    Initial Commit

---
+++ EquationCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EquationCalculator
+//
+//  Created by Владислав Закорко on 10.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
